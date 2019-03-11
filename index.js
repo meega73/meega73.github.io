e28118bbef900cc102f208591caa4cd443337c61
kranthikandi <kranthikumar.kandi@gmail.com>
2019-03-10 23:12:13 -0700

    add all the content

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
     var $header = $(".navbar"),
         $clone = $header.before($header.clone().addClass("clone"));
-    
-    $(window).on("scroll", function() {
+
+    $(window).on("scroll", function () {
         var fromTop = $(window).scrollTop();
         $("body").toggleClass("down", (fromTop > 400));
     });
 
     // ===== Scroll to Top ==== 
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-        $('#return-to-top').fadeIn(200);    // Fade in the arrow
-        $('#caption').hide();
-    } else {
-        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    }
-});
-$('#return-to-top').click(function() {      // When arrow is clicked
-    $('body,html').animate({
-        scrollTop : 0                       // Scroll to top of body
-    }, 500);
-});
+    $(window).scroll(function () {
+        if ($(this).scrollTop() >= 50) { // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200); // Fade in the arrow
+            $('#caption').hide();
+        } else {
+            $('#return-to-top').fadeOut(200); // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function () { // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0 // Scroll to top of body
+        }, 500);
+    });
 
 });
